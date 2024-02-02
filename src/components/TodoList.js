88671a8d9c6ab188dda5c4ae0130a6aceee3c85a
Abhishek Singh <abhishek2511.25@gmail.com>
2024-02-02 16:46:31 +0530

    adding frontend

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -55,3 +55,80 @@ function TodoList() {
 }
 
 export default TodoList;
+/*
+import React, { useState, useEffect } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/todos')
+            .then(response => response.json())
+            .then(data => setTodos(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    const addTodo = async todo => {
+        const response = await fetch('http://localhost:8080/api/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo),
+        });
+
+        const data = await response.json();
+        setTodos([...todos, data]);
+    };
+
+    const updateTodo = async (id, newValue) => {
+        const response = await fetch(`http://localhost:8080/api/todos/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newValue),
+        });
+
+        const data = await response.json();
+        setTodos(prev => prev.map(item => (item.id === data.id ? data : item)));
+    };
+
+    const removeTodo = async id => {
+        await fetch(`http://localhost:8080/api/todos/${id}`, {
+            method: 'DELETE',
+        });
+
+        setTodos(prev => prev.filter(todo => todo.id !== id));
+    };
+
+    const completeTodo = async id => {
+        const response = await fetch(`http://localhost:8080/api/todos/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ isComplete: true }),
+        });
+
+        const data = await response.json();
+        setTodos(prev => prev.map(todo => (todo.id === data.id ? data : todo)));
+    };
+
+    return (
+        <>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
+        </>
+    );
+}
+
+export default TodoList;*/
